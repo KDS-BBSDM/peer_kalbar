08dec1f5eca505518a750d6630cc6e29649c785a
Ovan Pulu <ovan89@gmail.com>
2015-08-04 08:49:24 +0700

    serverside taxon

--- services/controller/taxon.php
+++ services/controller/taxon.php
@@ -48,8 +48,7 @@ class taxon extends Controller {
 		$SSConfig = $this->$loadFunction();
 
 		$output = $this->serverSide->dTableData($SSConfig);
-		header('Access-Control-Allow-Origin: *');
-		header("Access-Control-Allow-Headers: *");
+		// $this->cors();exit;
 		echo json_encode($output);
 
 		exit;
@@ -128,6 +127,30 @@ class taxon extends Controller {
 		
 		return $SSConfig;
     }
+
+    function cors() {
+
+	    // Allow from any origin
+	    if (isset($_SERVER['HTTP_ORIGIN'])) {
+	        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+	        header('Access-Control-Allow-Credentials: true');
+	        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+	    }
+
+	    // Access-Control headers are received during OPTIONS requests
+	    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+	        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+	            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+	        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+	            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+	        exit(0);
+	    }
+
+	    echo "You have CORS!";
+	}
 
 	function getDataTaxon()
 	{
